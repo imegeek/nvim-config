d4c881e638f4301fdcd8daabf6b20d1b9c9bd1a0
abhackerofficial <abhackerofficial@gmail.com>
2021-06-03 22:08:49 +0530

    Update commit

---
+++ system/termux/nvim/bin/floaterm.cmd
@@ -0,0 +1,31 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+if "%1" == "" GOTO help
+if "%VIM_EXE%" == "" GOTO missing
+
+REM Get absolute name
+for /f "delims=" %%i in ("%1") do set "NAME=%%~fi"
+REM echo fullpath: %NAME%
+
+if "%NVIM_LISTEN_ADDRESS%" == "" GOTO vim
+goto neovim
+
+:vim
+call "%VIM_EXE%" --servername "%VIM_SERVERNAME%" --remote-expr "Tapi_edita_open(bufnr(), ['%NAME%'])"
+goto end
+
+:neovim
+%FLOATERM% %NAME%
+goto end
+
+:help
+echo usage: floaterm {filename}
+goto end
+
+:missing
+echo Must be called inside vim/neovim
+goto end
+
+
+:end
